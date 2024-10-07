d75e508019bd9fc9ec324ab5e34e604998ac55b7
Kuthuru <RithvikKuthuru@my.unt.edu>
2024-10-06 23:13:51 -0500

    [Rithvik Kuthuru] implemented luhn and testing

--- 1015-WWCCV.py
+++ 1015-WWCCV.py
@@ -15,4 +15,20 @@ card_number = input("Enter your 16-digit credit card number: ")
 if is_credit_card_valid(card_number):  # we are calling the function above and sending it the card_number to validate
     print("The credit card number is valid.")
 else:
-    print("The credit card number is invalid.")    
+    print("The credit card number is invalid.")
+
+def run_tests():
+
+    assert is_credit_card_valid("4111111111111111"), '4111111111111111 should pass but did not'
+
+    assert is_credit_card_valid("5105105105105100"), '5105105105105100 should pass but did not'
+
+    assert not is_credit_card_valid("134"), '134 should not pass but did'
+
+    assert not is_credit_card_valid("1234567890123456"), '1234567890123456 should not pass but did'
+
+    assert not is_credit_card_valid("000000000000"), 'This is a bad test and we will get an error message'
+
+run_tests()
+
+    
